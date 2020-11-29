cefde292efd7a54b62d2df95ed5f1746078ca135
Paul Hagwood <SubtleKnife@Pauls-MBP.attlocal.net>
2020-11-29 13:21:30 -0500

    Burgers Eaten Working

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,25 +1,25 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-  // $(".change-sleep").on("click", function(event) {
-  //   var id = $(this).data("id");
-  //   var newSleep = $(this).data("newsleep");
+  $(".devour-burger").on("click", function(event) {
+    var id = $(this).data("id");
+    var eatBurger = $(this).data("eatburger");
 
-  //   var newSleepState = {
-  //     sleepy: newSleep
-  //   };
+    var eatenBugerState = {
+      eaten: eatBurger
+    };
 
-  //   // Send the PUT request.
-  //   $.ajax("/api/burgers/" + id, {
-  //     type: "PUT",
-  //     data: newSleepState
-  //   }).then(
-  //     function() {
-  //       console.log("changed sleep to", newSleep);
-  //       // Reload the page to get the updated list
-  //       location.reload();
-  //     }
-  //   );
-  // });
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: eatenBugerState
+    }).then(
+      function() {
+        console.log("changed burger state to", eatBurger);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
 
   $(".create-form").on("submit", function(event) {
     // Make sure to preventDefault on a submit event.
@@ -43,18 +43,18 @@ $(function() {
     );
   });
 
-  $(".delete-cat").on("click", function(event) {
-    var id = $(this).data("id");
+  // $(".delete-cat").on("click", function(event) {
+  //   var id = $(this).data("id");
 
-    // Send the DELETE request.
-    $.ajax("/api/cats/" + id, {
-      type: "DELETE"
-    }).then(
-      function() {
-        console.log("deleted cat", id);
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
+  //   // Send the DELETE request.
+  //   $.ajax("/api/cats/" + id, {
+  //     type: "DELETE"
+  //   }).then(
+  //     function() {
+  //       console.log("deleted cat", id);
+  //       // Reload the page to get the updated list
+  //       location.reload();
+  //     }
+  //   );
+  // });
 });
